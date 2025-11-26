120e72cb9d8fb5cb898d6d211d8d0733106619e5
Stenver <stenver.lindmaa@gmail.com>
2025-11-26 18:10:36 +0200

    Update nav.php

--- phpIndex/nav.php
+++ phpIndex/nav.php
@@ -1,19 +1,25 @@
-<nav>
-    <ul>
-        <li>
-            <a href="?leht=kodu.php">Kodu</a>
-        </li>
-        <li>
-            <a href="?leht=jsToo.php">JS joonis</a>
-        </li>
-        <li>
-            <a href="?leht=ajafunktsioon.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-             <a href="?leht=muusikakusitlus.php">Muusika</a>
-        </li>
-        <li>
-            <a href="https://stenverlindma24.thkit.ee/" target="_blank">Vana index.html</a>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="et">
+<body>
+<div class="menu">
+    <div id="content">
+        <nav class="menu">
+            <ul>
+                <li><a href="?leht=kodu.php">Avaleht</a></li>
+                <li><a href="?leht=ajafunktsioon.php">Ajafunktsioonid</a>
+                    <li><a href="#">FUNKTSIOONID</a>
+                    <ul class="dropdown">
+                     <li><a href="https://stenverlindma24.thkit.ee">Kodulehed</a></li>
+                        <li><a href="?leht=muusikakusitlus.php">Muusika</a></li>
+                        <li><a href="?leht=Pildifailid.php">Pildid</a></li>
+                        <li><a href="?leht=matemaatilisedfunktsioonid.php">Matemaatilised funktsioonid</a></li>
+                        <li><a href="?leht=gitKasud.php">gitKasud</a></li>
+                    </ul>
+                 </li>
+             </ul>
+        </nav>
+    </div>
+</div>
+</body>
+</html>
+
