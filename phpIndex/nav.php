ce3a00f910d15c31d5a118f03f3cb3b91d7d9c5e
Stenver <stenver.lindmaa@gmail.com>
2025-12-01 12:26:10 +0200

    nav

--- phpIndex/nav.php
+++ phpIndex/nav.php
@@ -9,11 +9,27 @@
                 <li><a href="?leht=ajafunktsioon.php">Ajafunktsioonid</a>
                     <li><a href="#">FUNKTSIOONID</a>
                     <ul class="dropdown">
-                     <li><a href="https://stenverlindma24.thkit.ee">Kodulehed</a></li>
-                        <li><a href="?leht=muusikakusitlus.php">Muusika</a></li>
-                        <li><a href="?leht=Pildifailid.php">Pildid</a></li>
-                        <li><a href="?leht=matemaatilisedfunktsioonid.php">Matemaatilised funktsioonid</a></li>
-                        <li><a href="?leht=gitKasud.php">gitKasud</a></li>
+                     <li>
+                         <a href="https://stenverlindma24.thkit.ee">Kodulehed</a>
+                     </li>
+                        <li>
+                            <a href="?leht=muusikakusitlus.php">Muusika</a>
+                        </li>
+                        <li>
+                            <a href="?leht=Pildifailid.php">Pildid</a>
+                        </li>
+                        <li>
+                            <a href="?leht=matemaatilisedfunktsioonid.php">Matemaatilised funktsioonid</a>
+                        </li>
+                        <li>
+                            <a href="?leht=gitKasud.php">gitKasud</a>
+                        </li>
+                        <li>
+                            <a href="content/andmebaasLoomad/loomadeKuvamine.php" target="_blank">Loomad AB</a>
+                        </li>
+                        <li>
+                            <a href="/content/mobiiliamll/blankett.html.php" target="_blank">Mobiilimall</a>
+                        </li>
                     </ul>
                  </li>
              </ul>
