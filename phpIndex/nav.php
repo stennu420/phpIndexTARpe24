6447f4df2e0872540d50df9273170783a6c0ee78
Stenver <stenver.lindmaa@gmail.com>
2025-12-19 12:14:13 +0200

    Refactor navigation structure in nav.php

--- phpIndex/nav.php
+++ phpIndex/nav.php
@@ -4,38 +4,25 @@
 <div class="menu">
     <div id="content">
         <nav class="menu">
+
             <ul>
-                <li><a href="?leht=kodu.php">Avaleht</a></li>
-                <li><a href="?leht=ajafunktsioon.php">Ajafunktsioonid</a>
-                    <li><a href="#">FUNKTSIOONID</a>
+                <li><a href="content/kodu.php">Kodu</a></li>
+                <li><a href="https://stenverlindma24.thkit.ee" target="_blank">Vana Index</a></li>
+                <li><a href="content/gitKasud.php">gitKasud</a></li>
+                <li><a href="content/JStoo.php">JStoo</a></li>
+                <li><a href="content/mobiilimall/blankett.html.php">Mobiilimall</a></li>
+                <li><a href="content/anektoot/anektootp2is.php">Anekdoot</a></li>
+                <li><a href="content/MoodlePHPraamat/avaleht.php">Väikefirma</a></li>
+                <li><a href="#">FUNKTSIOONID</a>
                     <ul class="dropdown">
-                     <li>
-                         <a href="https://stenverlindma24.thkit.ee">Kodulehed</a>
-                     </li>
-                        <li>
-                            <a href="?leht=muusikakusitlus.php">Muusika</a>
-                        </li>
-                        <li>
-                            <a href="?leht=Pildifailid.php">Pildid</a>
-                        </li>
-                        <li>
-                            <a href="?leht=matemaatilisedfunktsioonid.php">Matemaatilised funktsioonid</a>
-                        </li>
-                        <li>
-                            <a href="?leht=gitKasud.php">gitKasud</a>
-                        </li>
-                        <li>
-                            <a href="content/andmebaasLoomad/loomadeKuvamine.php" target="_blank">Loomad AB</a>
-                        </li>
-                        <li>
-                            <a href="/content/mobiiliamll/blankett.html.php" target="_blank">Mobiilimall</a>
-                        </li>
+                        <li><a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a></li>
+                        <li><a href="?leht=tekstifunktsioonid.php">Tekstfunktsioonid</a></li>
+                        <li><a href="?leht=matemaatilisedfunktsioonid.php">Matemaatilised Funktsioonid</a></li>
                     </ul>
-                 </li>
-             </ul>
+                </li>
+            </ul>
         </nav>
     </div>
 </div>
 </body>
 </html>
-
